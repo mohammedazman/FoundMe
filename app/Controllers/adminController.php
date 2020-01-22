67d3be21083ff87eae690773f9684d916a6945f9
ibtehalFahd <57089551+ibtehalFahd@users.noreply.github.com>
2020-01-22 10:04:59 +0300

    addToDadh

--- app/Controllers/adminController.php
+++ app/Controllers/adminController.php
@@ -5,33 +5,22 @@
 class adminController extends Controller
 {
 
-
   public function index($id='',$name='')
   {
-
 
     $this->view('cp'.DIRECTORY_SEPARATOR.'index');
     $this->view->pageTitle='Found Me Admin';
     $this->view->render();
 
   }
+  public function users()
+  {
 
+    $this->view('cp'.DIRECTORY_SEPARATOR.'users');
+    $this->view->pageTitle='Found Me Admin';
+    $this->view->render();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  }
 
 
 }
